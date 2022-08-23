455fb8b33769fffbdab9d219402f2775bf07f539
riztechverx26 <muhammad.rizwan@techverx.com>
2022-08-23 14:38:53 +0500

    yu

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-	for (int i = 0; i < 9; i++) 
+	for (int i = 0; i < 91; i++) 
 {
   System.out.println("Hello, World! M" + i); 
 }
